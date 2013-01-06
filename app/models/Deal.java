ccd618b973c9512951d4e5516f2f2776f2e3f997
juacompe <juacompe@gmail.com>
2013-01-06 23:20:08 +0700

    add Deal model

---
+++ app/models/Deal.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+import javax.persistence.Entity;
+import play.db.jpa.Model;
+/**
+ *
+ * @author juacompe
+ */
+@Entity
+class Deal extends Model {
+
+    String title;
+    double price;
+    
+    Deal(String title, double price) {
+        this.title = "Korean tour";
+        this.price = 80000;
+    }
+    
+}
---
+++ test/models/TestDeal.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+import org.junit.Test;
+import play.test.UnitTest;
+
+/**
+ *
+ * @author juacompe
+ */
+public class TestDeal extends UnitTest {
+    
+    @Test
+    public void createNewDeal() {
+        String title = "Korean tour";
+        double price = 80000;
+        Deal deal = new Deal(title, price);
+        assertEquals(title, deal.title);
+        assertEquals(price, deal.price, 0);
+    }
+    
+    @Test 
+    public void saveAndRetrieveDeal() {
+        String title = "Korean tour";
+        double price = 80000;
+        Deal deal = new Deal(title, price);
+        deal.save();
+        
+        Deal d = Deal.findById(deal.id);
+        assertEquals(title, d.title);
+        assertEquals(price, d.price, 0);
+    }
+
+}
